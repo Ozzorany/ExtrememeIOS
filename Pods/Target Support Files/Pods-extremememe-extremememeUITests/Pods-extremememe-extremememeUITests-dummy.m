adc853b4619439c5b431ad9bd7b728d3899603a8
ozzorany <ozzeshmi@gmail.com>
2021-05-23 14:11:15 -0700

    add pod file

---
+++ Pods/Target Support Files/Pods-extremememe-extremememeUITests/Pods-extremememe-extremememeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_extremememe_extremememeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_extremememe_extremememeUITests
+@end
---
+++ Pods/Target Support Files/Pods-extremememeTests/Pods-extremememeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_extremememeTests : NSObject
+@end
+@implementation PodsDummy_Pods_extremememeTests
+@end
